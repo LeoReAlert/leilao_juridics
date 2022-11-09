caf48a355a7a69d597a7d42acabce1b3b8ffc1d6
Leonardo <leonardo@localhost.localdomain>
2022-11-09 18:02:26 -0400

    feat: create inicial steps

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,3 +49,34 @@
 
   </head>
    <body class="g-sidenav-show  bg-gray-100">
+
+   @include('layouts.aside')
+
+   @include('layouts.navbar')
+
+   @include('admin.pages.checking')
+
+   @include('layouts.footer')
+   <!--   Core JS Files   -->
+<script src="./assets/js/core/popper.min.js" ></script>
+<script src="./assets/js/core/bootstrap.min.js" ></script>
+<script src="./assets/js/plugins/perfect-scrollbar.min.js" ></script>
+<script src="./assets/js/plugins/smooth-scrollbar.min.js" ></script>
+   <script>
+  var win = navigator.platform.indexOf('Win') > -1;
+  if (win && document.querySelector('#sidenav-scrollbar')) {
+    var options = {
+      damping: '0.5'
+    }
+    Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+  }
+</script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+
+<!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc --><script src="./assets/js/material-dashboard.min.js?v=3.0.4"></script>
+  </body>
+
+</html>
